292526a16eb8249f32ec4ca0e0a069b9592a84b3
Abdullah <atmughrabi@gmail.com>
2022-12-06 22:18:36 -0500

    [RTL] fix packaging script

--- 01_device/scripts/glay_kernel_filelist.f
+++ 01_device/scripts/glay_kernel_filelist.f
@@ -1,30 +1,13 @@
-./IP/iob_cache/iob_cache_stub.v
-./IP/iob_cache/iob_fifo_sync/iob_fifo_sync.v
 ./IP/iob_cache/iob_include/iob-cache.vh
 ./IP/iob_cache/iob_include/iob_lib.vh
-./IP/iob_cache/iob_ram_2p_asym/iob_ram_2p_asym.v
-./IP/iob_cache/iob_ram_2p/iob_ram_2p.v
-./IP/iob_cache/iob_ram_sp/iob_ram_sp.v
-./IP/iob_cache/iob_regfile_sp/iob_regfile_sp.v
-./IP/iob_cache/iob_src_cache/back-end-axi.v
-./IP/iob_cache/iob_src_cache/back-end-native.v
-./IP/iob_cache/iob_src_cache/cache-control.v
-./IP/iob_cache/iob_src_cache/cache-memory.v
-./IP/iob_cache/iob_src_cache/front-end.v
-./IP/iob_cache/iob_src_cache/iob-cache-axi.v
-./IP/iob_cache/iob_src_cache/iob_cache.v
-./IP/iob_cache/iob_src_cache/onehot-to-bin-encoder.v
-./IP/iob_cache/iob_src_cache/read-channel-axi.v
-./IP/iob_cache/iob_src_cache/read-channel-native.v
-./IP/iob_cache/iob_src_cache/replacement-policy.v
-./IP/iob_cache/iob_src_cache/write-channel-axi.v
-./IP/iob_cache/iob_src_cache/write-channel-native.v
 
-./IP/glay_kernel/glay_kernel_afu.sv
-./IP/glay_kernel/glay_kernel_cu.sv
 ./IP/glay_pkgs/glay_axi4_pkg.sv
 ./IP/glay_pkgs/glay_descriptor_pkg.sv
 ./IP/glay_pkgs/glay_globals_pkg.sv
+
+./IP/glay_kernel/glay_kernel_afu.sv
+./IP/glay_kernel/glay_kernel_cu.sv
+
 ./IP/glay_top/glay_top_control_s_axi.v
 ./IP/glay_top/glay_top.v
 
