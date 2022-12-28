b2fcdfa17d07313d4375126eb44b74801b78a7ad
Abdullah <atmughrabi@gmail.com>
2022-12-27 20:01:08 -0500

    [RTL] reindent

--- 01_device/IP/iob_cache/iob_src_cache/back-end-axi.v
+++ 01_device/IP/iob_cache/iob_src_cache/back-end-axi.v
@@ -14,7 +14,7 @@ module back_end_axi
     parameter FE_NBYTES  = FE_DATA_W/8,        //Number of Bytes per Word
     parameter FE_BYTE_W  = $clog2(FE_NBYTES), //Byte Offset
     /*---------------------------------------------------*/
-    //Higher hierarchy memory (slave) interface parameters 
+    //Higher hierarchy memory (slave) interface parameters
 
     parameter BE_NBYTES = BE_DATA_W/8, //Number of bytes
     parameter BE_BYTE_W = $clog2(BE_NBYTES), //Offset of Number of Bytes
@@ -28,8 +28,8 @@ module back_end_axi
     parameter AXI_ID_W              = 1, //AXI ID (identification) width
     parameter AXI_LEN_W             = 8, //AXI ID burst length (log2)
     parameter [AXI_ID_W-1:0] AXI_ID = 0  //AXI ID value
-    ) 
-   (
+  )
+  (
     //write-through-buffer
     input                                                                    write_valid,
     input [FE_ADDR_W-1:FE_BYTE_W + WRITE_POL*WORD_OFF_W]                     write_addr,
@@ -45,64 +45,58 @@ module back_end_axi
     output [BE_DATA_W -1:0]                                                  read_rdata,
 
     //AXI master backend interface
-`include "m_axi_m_port.vh"
+    `include "m_axi_m_port.vh"
     input                                                                    clk,
     input                                                                    reset
-    );
+  );
 
 
-   
-   read_channel_axi
-     #(
-       .FE_ADDR_W(FE_ADDR_W),
-       .FE_DATA_W(FE_DATA_W),  
-       .WORD_OFF_W(WORD_OFF_W),
-       .BE_ADDR_W (BE_ADDR_W),
-       .BE_DATA_W (BE_DATA_W),
-       .AXI_ADDR_W(AXI_ADDR_W),
-       .AXI_DATA_W(AXI_DATA_W),
-       .AXI_ID_W(AXI_ID_W),
-       .AXI_LEN_W(AXI_LEN_W),
-       .AXI_ID  (AXI_ID)
-       )
-   read_fsm
-     (
-      .replace_valid (replace_valid),
-      .replace_addr (replace_addr),
-      .replace (replace),
-      .read_valid (read_valid),
-      .read_addr (read_addr),
-      .read_rdata (read_rdata),
-      `include "m_axi_read_portmap.vh"
-      .clk(clk),
-      .reset(reset)
-      );
 
-   
-   write_channel_axi
-     #(
-       .FE_ADDR_W(FE_ADDR_W),
-       .FE_DATA_W(FE_DATA_W),
-       .BE_ADDR_W (BE_ADDR_W),
-       .BE_DATA_W (BE_DATA_W),
-       .WRITE_POL (WRITE_POL),
-       .WORD_OFF_W(WORD_OFF_W),
-       .AXI_ADDR_W(AXI_ADDR_W),
-       .AXI_DATA_W(AXI_DATA_W),
-       .AXI_LEN_W(AXI_LEN_W),
-       .AXI_ID_W(AXI_ID_W),
-       .AXI_ID(AXI_ID)
-       )
-   write_fsm
-     (
-      .valid (write_valid),
-      .addr (write_addr),
-      .wstrb (write_wstrb),
-      .wdata (write_wdata),
-      .ready (write_ready),
-      `include "m_axi_write_portmap.vh"
-      .clk(clk),
-      .reset(reset)
-      );
-   
+read_channel_axi #(
+  .FE_ADDR_W (FE_ADDR_W ),
+  .FE_DATA_W (FE_DATA_W ),
+  .WORD_OFF_W(WORD_OFF_W),
+  .BE_ADDR_W (BE_ADDR_W ),
+  .BE_DATA_W (BE_DATA_W ),
+  .AXI_ADDR_W(AXI_ADDR_W),
+  .AXI_DATA_W(AXI_DATA_W),
+  .AXI_ID_W  (AXI_ID_W  ),
+  .AXI_LEN_W (AXI_LEN_W ),
+  .AXI_ID    (AXI_ID    )
+) read_fsm (
+  .replace_valid(replace_valid),
+  .replace_addr (replace_addr ),
+  .replace      (replace      ),
+  .read_valid   (read_valid   ),
+  .read_addr    (read_addr    ),
+  .read_rdata   (read_rdata   ),
+  `include "m_axi_read_portmap.vh"
+  .clk          (clk          ),
+  .reset        (reset        )
+);
+
+
+write_channel_axi #(
+  .FE_ADDR_W (FE_ADDR_W ),
+  .FE_DATA_W (FE_DATA_W ),
+  .BE_ADDR_W (BE_ADDR_W ),
+  .BE_DATA_W (BE_DATA_W ),
+  .WRITE_POL (WRITE_POL ),
+  .WORD_OFF_W(WORD_OFF_W),
+  .AXI_ADDR_W(AXI_ADDR_W),
+  .AXI_DATA_W(AXI_DATA_W),
+  .AXI_LEN_W (AXI_LEN_W ),
+  .AXI_ID_W  (AXI_ID_W  ),
+  .AXI_ID    (AXI_ID    )
+) write_fsm (
+  .valid(write_valid),
+  .addr (write_addr ),
+  .wstrb(write_wstrb),
+  .wdata(write_wdata),
+  .ready(write_ready),
+  `include "m_axi_write_portmap.vh"
+  .clk  (clk        ),
+  .reset(reset      )
+);
+
 endmodule // back_end_native
