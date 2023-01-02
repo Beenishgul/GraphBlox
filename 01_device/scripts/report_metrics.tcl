f481b6d4f6dcad4e3102f5551da6b01864478135
Abdullah <atmughrabi@gmail.com>
2023-01-01 21:52:11 -0500

    [SCRIPTS] clean up make file add metrics script

---
+++ 01_device/scripts/report_metrics.tcl
@@ -0,0 +1,8 @@
+open_run impl_1
+#Utilisation
+report_utilization -file utilization_hierarchical.txt -hierarchical_depth 2 -hierarchical
+report_utilization -file utilization_report.txt
+
+#Power
+report_power -file power_hierarchical.txt -hierarchical_depth 3 
+report_power -file {power.txt} -xpe {power.xpe} -rpx {power.rpx}
