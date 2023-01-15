957bc0d48c4da999f4d15f984d18b21bdc2a245f
Abdullah <atmughrabi@gmail.com>
2023-01-15 10:11:31 -0500

    [SCRIPTS] add perl script to minimize synth process output

--- 01_device/scripts/color_tail.pl
+++ 01_device/scripts/color_tail.pl
@@ -3,8 +3,158 @@
 use IO::Handle qw( );
 use Term::ANSIColor qw(:constants);
 
-$progress_flag = 0;
-$time_flag = 0;
+
+$time_flag     = 0;
+@error_array   = ();
+@warning_array = ();
+$datestring    = localtime();
+$step_cnt      = 0;
+
+$reports_file = "Reports:Dir";
+$log_file = "Reports:Dir";
+$guidance_file = "Reports:Dir";
+$steps_file = "Reports:Dir";
+
+$| = 1; # unbuffer output
+
+$spinner = 0;
+@spinState = ("-", "\\", "|", "/");
+
+sub status()
+{
+    for (my $x = 0; $x <= 100; ++$x){
+     $spinner = ($spinner + 1) % 4;
+     print "\b"; sleep 0.2; print "$spinState[$spinner]";
+ }
+}
+
+# Time (s): cpu = 00:00:00 ; elapsed = 00:00:00 . Memory (MB): peak = 469.250 ; gain = 0.000 ; free physical = 49860 ; free virtual = 115857
+
+sub sub_print_time {
+ my $line = $_;
+ $time_flag = 1;
+
+ my @fields_t = split ';', $line;
+   # my @fields_t2 = split ':', @fields_i2[0];
+   
+   print "                        ";
+   print "[", CYAN, "TIME" , RESET, "]"; 
+   print GREEN, $fields_t[1] , RESET,"\n"; 
+   # print GREEN, @fields_i2[1] , RESET;   
+}
+
+sub sub_print_error {
+ my $line = $_;
+
+   # print "\n", RED, $line, RESET;  
+   $time_flag = 1;
+   push(@error_array, $line);
+}
+
+sub sub_print_warning {
+ my $line = $_;
+
+   # print "\n", YELLOW, $line, RESET;  
+   $time_flag = 1;
+   push(@warning_array, $line);
+}
+
+sub sub_print_progress {
+ my $line = $_;
+   # status()
+   print RED, ">", RESET;
+}
+
+sub sub_print_progress_s {
+ my $line = $_;
+
+   # print RED, "*", RESET;
+}
+
+sub sub_print_step {
+ my $line = $_;
+ $step_cnt++;
+
+ print "                ";
+ print YELLOW,"Step ", $step_cnt, ": ";
+ print $line, RESET;  
+ $time_flag = 0;
+}
+
+sub sub_print_close {
+ my $line = $_;
+ $step_cnt++;
+
+ print "                ";
+ print YELLOW,"Step ", $step_cnt, ": ";
+ print $line, RESET;  
+ $time_flag = 0;
+
+ my @fields_r = split ':', $reports_file;
+ my @fields_l = split ':', $log_file;
+ my @fields_g = split ':', $guidance_file;
+ my @fields_s = split ':', $steps_file;
+
+ print "\n", "=========================================================", "\n";
+ print "[", BLUE, "Synthesis And Implementation For Target Kernel", RESET, "]";
+ print RED, " DONE!", RESET, "\n";
+ print "=========================================================", "\n";
+ print "[", GREEN, "$datestring",RESET,"\n";
+ print "=========================================================", "\n";
+ print "[", BLUE, "Number of warning...", RESET, "] ";
+ print  YELLOW, scalar(@warning_array), RESET, "\n";
+ print "[", BLUE, "Number of errors...", RESET, "] ";
+ print  RED, scalar(@error_array), RESET, "\n";
+ print "=========================================================", "\n";
+ print "[", BLUE, "Reports..........", RESET, "] ";
+ print  YELLOW, $fields_r[1], RESET, "\n";
+ print "[", BLUE, "Log files........", RESET, "] ";
+ print  YELLOW, $fields_l[1], RESET, "\n";
+ print "[", BLUE, "Guidance.........", RESET, "] ";
+ print  YELLOW, $fields_g[1], RESET, "\n";
+ print "[", BLUE, "Steps Log File...", RESET, "] ";
+ print  YELLOW, $fields_s[1], RESET, "\n";
+ print "=========================================================", "\n";
+
+ exit 0;
+}
+
+sub sub_print_info {
+ my $line = $_;
+ $time_flag = 0;
+ my @fields_i2 = split ':', $line;
+ my @fields_i = split '/', $fields_i2[1];
+
+
+ print "                        ";
+ print "[", BLUE, $fields_i2[0], RESET, "] "; 
+ shift(@fields_i2);
+ print GREEN, @fields_i2, RESET;  
+ 
+}
+
+sub sub_print_substep {
+ my $line = $_;
+
+ print "                        ";
+ print BLUE, $line, RESET;  
+ $time_flag = 0;;
+}
+
+sub sub_print_vpl {
+ my $line = $_;
+
+ print "                        ";
+ print MAGENTA, $line, RESET;  
+ $time_flag = 0;;
+}
+
+print "\n", "=========================================================", "\n";
+print "[", BLUE, "Synthesis And Implementation For Target Kernel", RESET, "]";
+print RED, " START!", RESET, "\n";
+print "=========================================================", "\n";
+print "[", GREEN, "$datestring",RESET,"]","\n";
+print "=========================================================", "\n";
 
 while(<STDIN>) {
     my $line = $_;
@@ -16,49 +166,49 @@ while(<STDIN>) {
 
     my @fields_s = split ' ', $line;
 
+
+
+    # print "\nline     --> ", $line, "\n";
+    # print "fields_s --> ",scalar(@fields_s)," -- ", join('+',@fields_s),"\n";
+    # print "=========================================================", "\n";
+
     my $step_0 = "Running";
     my $step_1 = "Create";
     my $step_2 = "Run";
     my $step_3 = "Creating";
-    my $step_4 = "Closing";
-    my $step_5 = "\tReports:";
-    my $step_6 = "\tLog";
-    my $step_7 = "\tGuidance:";
-    my $step_8 = "\tSteps";
-    my $vpl = /^(\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9])\]/;
-
-    if($progress_flag == 0){
-        print "\n";
-    } 
-
-    if($time_flag == 1){
-       print "\n", BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;
-    } else {
+    my $step_4 = "Closing dispatch client";
+    my $step_5 = "Reports:";
+    my $step_6 = "Log";
+    my $step_7 = "Guidance:";
+    my $step_8 = "Steps";
+    my $step_9 = "Section:";
+    # my $vpl = \[[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\];
 
         if (@fields_s) {
             if ($fields_s[0] ne "")
             {
-                if    ($fields_s[0] =~ /^INFO:/   )  { print BRIGHT_RED, "=", RESET; $progress_flag = 1;}
-                elsif ($fields_s[0] =~ /^ERROR:/  )  { print "\n", RED, $line, RESET; $progress_flag  = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^WARNING:/)  { print "\n", YELLOW, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^Time/    )  { print "\n", GREEN, $line, RESET;  $progress_flag = 0; $time_flag = 1;}
-                elsif ($fields_s[0] =~ $vpl       )  { print BRIGHT_RED, "=", RESET; $progress_flag = 1;}
-                elsif ($fields_s[0] =~ /^$step_0/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_1/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_2/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_3/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_4/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_5/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_6/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_7/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                elsif ($fields_s[0] =~ /^$step_8/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
-                else                                 { print "";}
-            } 
-        } 
+                if    ($fields_s[0] =~ /^\*/   )       { }
+                elsif ($fields_s[0] =~ /^$step_0$/ )   { sub_print_step($line);}
+                elsif ($fields_s[0] =~ /^$step_1$/ )   { sub_print_step($line);}
+                elsif ($fields_s[0] =~ /^$step_2$/ )   { sub_print_step($line);}
+                elsif ($fields_s[0] =~ /^$step_3$/ )   { sub_print_step($line);}
+                elsif ($line =~ /$step_4/ )            { sub_print_close($line);}
+                elsif ($fields_s[0] =~ /^$step_5$/ )   { $reports_file = $line;}
+                elsif ($fields_s[0] =~ /^$step_6$/ )   { $log_file = $line;}
+                elsif ($fields_s[0] =~ /^$step_7$/ )   { $guidance_file = $line;}
+                elsif ($fields_s[0] =~ /^$step_8$/ )   { $steps_file = $line;}
+                elsif ($fields_s[0] =~ /^$step_9$/ )   { sub_print_substep($line);}
+                elsif ($fields_s[0] =~ /^INFO:/   )    { if($time_flag == 1){sub_print_info($line);} }
+                elsif ($fields_s[0] =~ /^ERROR:/  )    { sub_print_error ($line);}
+                elsif ($fields_s[0] =~ /^WARNING:/)    { sub_print_warning ($line);}
+                elsif ($fields_s[0] =~ /^Time/    )    { sub_print_time ($line);}
+                elsif ($fields_s[0] =~ /^\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\]$/       )  { sub_print_vpl();}
+                else                                 { }       
 
-    }
+                 # STDOUT->flush();
+             } 
+         } 
+  
+ }
 
-    STDOUT->flush();
-    
-}
 
