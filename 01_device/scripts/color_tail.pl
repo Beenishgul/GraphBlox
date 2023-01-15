8b091dbbbc360fdcff629c118c7a80f14ff8a8dd
Abdullah <atmughrabi@gmail.com>
2023-01-15 01:36:09 -0500

    [SCRIPTS] synth output reduced

--- 01_device/scripts/color_tail.pl
+++ 01_device/scripts/color_tail.pl
@@ -21,8 +21,10 @@ while(<STDIN>) {
     my $step_2 = "Run";
     my $step_3 = "Creating";
     my $step_4 = "Closing";
-    my $step_5 = "Reports:";
-    my $step_6 = "Log";
+    my $step_5 = "\tReports:";
+    my $step_6 = "\tLog";
+    my $step_7 = "\tGuidance:";
+    my $step_8 = "\tSteps";
     my $vpl = /^(\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9])\]/;
 
     if($progress_flag == 0){
@@ -48,6 +50,8 @@ while(<STDIN>) {
                 elsif ($fields_s[0] =~ /^$step_4/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
                 elsif ($fields_s[0] =~ /^$step_5/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
                 elsif ($fields_s[0] =~ /^$step_6/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_7/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_8/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
                 else                                 { print "";}
             } 
         } 
