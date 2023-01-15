5ec1a8f36ee0da500bf6458eeef2c5576cfef581
Abdullah <atmughrabi@gmail.com>
2023-01-15 01:26:50 -0500

    [SCRIPTS] synth output reduced

--- 01_device/scripts/color_tail.pl
+++ 01_device/scripts/color_tail.pl
@@ -1,35 +1,60 @@
 #!/usr/bin/perl -w
 
-#!/usr/bin/perl -w
+use IO::Handle qw( );
+use Term::ANSIColor qw(:constants);
+
+$progress_flag = 0;
+$time_flag = 0;
 
 while(<STDIN>) {
     my $line = $_;
     chomp($line);
 
     for($line){
-        s/\[.*\]/\e[1;34m$&\e[0m/g;   #vivdo operation in blue
-        s/ERROR:/\e[0;31m$&\e[0m/g;   #vivdo errors & stacktraces in red
-        s/INFO:/\e[1;32m$&\e[0m/g;    #info replacement in green
-        s/WARNING:/\e[1;33m$&\e[0m/g; #warning replacement in yellow
-        s/Time \(s\):/\e[0;35m$&\e[0m/g;#vivdo
-        s/Run vpl/\e[0;35m$&\e[0m/g;   #vivdo
-        s/Creating Vivado project/\e[0;31m$&\e[0m/g;   #vivdo errors & stacktraces in red
-        s/Running Dispatch.*/\e[0;31m$&\e[0m/g;   #vivdo errors & stacktraces in red
-        s/Closing dispatch client/\e[0;31m$&\e[0m/g;   #vivdo errors & stacktraces in red
-        s/Successfully/\e[1;32m$&\e[0m/g;    #info replacement in green
-        s/Log files:/\e[0;35m$&\e[0m/g; #vivdo
-        s/Reports:/\e[1;35m$&\e[0m/g; #warning replacement in blue
-        s/Guidance:/\e[1;35m$&\e[0m/g; #warning replacement in yellow
-        s/Steps Log File:/\e[1;35m$&\e[0m/g; #warning replacement in blue
+        s/.*/$&/g;   #vivdo operation in blue
     }
 
-    my @fields = split /[\.\/]/, $line;
+    my @fields_s = split ' ', $line;
 
-    if (@fields) {
-        if ($fields[0] ne "")
-        {
-           print $fields[0], "\n";
-        }
+    my $step_0 = "Running";
+    my $step_1 = "Create";
+    my $step_2 = "Run";
+    my $step_3 = "Creating";
+    my $step_4 = "Closing";
+    my $step_5 = "Reports:";
+    my $step_6 = "Log";
+    my $vpl = /^(\[[0-9][0-9]:[0-9][0-9]:[0-9][0-9])\]/;
+
+    if($progress_flag == 0){
+        print "\n";
+    } 
+
+    if($time_flag == 1){
+       print "\n", BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;
+    } else {
+
+        if (@fields_s) {
+            if ($fields_s[0] ne "")
+            {
+                if    ($fields_s[0] =~ /^INFO:/   )  { print BRIGHT_RED, "=", RESET; $progress_flag = 1;}
+                elsif ($fields_s[0] =~ /^ERROR:/  )  { print "\n", RED, $line, RESET; $progress_flag  = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^WARNING:/)  { print "\n", YELLOW, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^Time/    )  { print "\n", GREEN, $line, RESET;  $progress_flag = 0; $time_flag = 1;}
+                elsif ($fields_s[0] =~ $vpl       )  { print BRIGHT_RED, "=", RESET; $progress_flag = 1;}
+                elsif ($fields_s[0] =~ /^$step_0/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_1/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_2/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_3/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_4/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_5/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                elsif ($fields_s[0] =~ /^$step_6/ )  { print "\n", BRIGHT_BLUE, $line, RESET;  $progress_flag = 0; $time_flag = 0;}
+                else                                 { print "";}
+            } 
+        } 
+
     }
-   
+
+    STDOUT->flush();
+    
 }
+
