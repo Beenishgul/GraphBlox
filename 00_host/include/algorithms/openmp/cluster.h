f662e7898965770c666ca63f9a20690610e3e56d
Abdullah <atmughrabi@gmail.com>
2023-02-09 17:52:27 -0500

    [GLAY|CLUSTERING] add louvain clustering algorithm

---
+++ 00_host/include/algorithms/openmp/cluster.h
@@ -0,0 +1,73 @@
+#ifndef LOUVAIN_H
+#define LOUVAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "graphConfig.h"
+#include "arrayQueue.h"
+#include "bitmap.h"
+#include "graphCSR.h"
+
+
+#define K 5
+#define MIN_IMPROVEMENT 0.005
+
+// ********************************************************************************************
+// ***************                  Stats DataStructure                          **************
+// ********************************************************************************************
+
+struct ClusterStats
+{
+    int *parents;
+    uint32_t  processed_nodes;
+    uint32_t  num_vertices;
+    uint32_t iteration;
+    double time_total;
+};
+
+struct ClusterPartition{
+  // size of the partition
+  uint32_t size;
+  
+  // community to which each node belongs
+  uint32_t *node2Community;
+  
+  // in and tot values of each node to compute modularity 
+  long double *in;
+  long double *tot;
+  
+  // utility arrays to find communities adjacent to a node
+  // communities are stored using three variables
+  // - neighCommWeights: stores weights to communities
+  // - neighCommPos: stores list of neighbor communities
+  // - neighCommNb: stores the number of neighbor communities
+  long double *neighCommWeights;
+  uint32_t *neighCommPos;
+  uint32_t neighCommNb;
+};
+
+
+struct ClusterStats *newClusterStatsGraphCSR(struct GraphCSR *graph);
+struct ClusterPartition *newClusterPartitionGraphCSR(struct GraphCSR *graph);
+
+void freeClusterStats(struct ClusterStats *stats);
+void freeClusterPartition(struct ClusterPartition *partition);
+
+// ********************************************************************************************
+// ***************                  CSR DataStructure                            **************
+// ********************************************************************************************
+
+struct ClusterStats *clusterGraphCSR(struct Arguments *arguments, struct GraphCSR *graph);
+
+struct ClusterStats *louvainGraphCSR(struct Arguments *arguments, struct GraphCSR *graph);
+struct ClusterStats *leidenGraphCSR(struct Arguments *arguments, struct GraphCSR *graph);
+struct ClusterStats *incrementalAggregationGraphCSR(struct Arguments *arguments, struct GraphCSR *graph);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
