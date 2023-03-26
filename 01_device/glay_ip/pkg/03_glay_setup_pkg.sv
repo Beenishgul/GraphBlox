93794bcb153e4a3299e4dda5f7c1a7909a94d248
Abdullah <atmughrabi@gmail.com>
2023-03-26 00:40:32 -0400

    [GraphBlox|RTL] update reset signals in setup/read engines

---
+++ 01_device/glay_ip/pkg/03_glay_setup_pkg.sv
@@ -0,0 +1,34 @@
+// -----------------------------------------------------------------------------
+//
+//      "GLay: A Vertex Centric Re-Configurable Graph Processing Overlay"
+//
+// -----------------------------------------------------------------------------
+// Copyright (c) 2021-2022 All rights reserved
+// -----------------------------------------------------------------------------
+// Author : Abdullah Mughrabi atmughrabi@gmail.com/atmughra@virginia.edu
+// File   : glay_setup_pkg.sv
+// Create : 2022-11-29 16:14:59
+// Revise : 2022-11-29 16:14:59
+// Editor : sublime text4, tab size (4)
+// -----------------------------------------------------------------------------
+
+
+`timescale 1 ns / 1 ps
+package GLAY_SETUP_PKG;
+
+import GLAY_GLOBALS_PKG::*;
+
+// --------------------------------------------------------------------------------------
+//   State Machine Setup Requests
+// --------------------------------------------------------------------------------------
+
+typedef enum int unsigned {
+  SETUP_KERNEL_RESET,
+  SETUP_KERNEL_IDLE,
+  SETUP_KERNEL_REQ_START,
+  SETUP_KERNEL_REQ_BUSY,
+  SETUP_KERNEL_REQ_DONE
+} kernel_setup_state;
+
+
+endpackage
