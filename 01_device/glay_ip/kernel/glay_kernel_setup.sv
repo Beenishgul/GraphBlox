4274a7fb86594bc7e96b0c776245d6cbe2061e53
Abdullah <atmughrabi@gmail.com>
2023-03-30 18:12:32 -0400

    [GraphBlox|RTL] fix interfacing a read engine

--- 01_device/glay_ip/kernel/glay_kernel_setup.sv
+++ 01_device/glay_ip/kernel/glay_kernel_setup.sv
@@ -208,7 +208,7 @@ module glay_kernel_setup #(
             end
             SETUP_KERNEL_REQ_BUSY : begin
                 glay_setup_cache_req_out_din.valid           <= 1'b0;
-                kernel_setup_done                            <= serial_read_engine_out_done_reg & ~serial_read_engine_fifo_out_signals_reg.empty;
+                kernel_setup_done                            <= serial_read_engine_out_done_reg & serial_read_engine_fifo_out_signals_reg.empty;
                 kernel_setup_start                           <= 1'b1;
                 serial_read_engine_fifo_in_signals_reg.rd_en <= ~serial_read_engine_fifo_out_signals_reg.empty;
                 serial_read_engine_in_start_reg              <= 1'b1;
