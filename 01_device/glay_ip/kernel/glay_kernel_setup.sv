78624f8e467cfacb478ddc3beb90bafcb5578de6
Abdullah <atmughrabi@gmail.com>
2023-03-31 02:04:00 -0400

    [GraphBlox|RTL] fix interfacing a read engine

--- 01_device/glay_ip/kernel/glay_kernel_setup.sv
+++ 01_device/glay_ip/kernel/glay_kernel_setup.sv
@@ -156,13 +156,13 @@ module glay_kernel_setup #(
                 next_state = SETUP_KERNEL_IDLE;
             end
             SETUP_KERNEL_IDLE : begin
-                if(glay_descriptor_reg.valid)
+                if(glay_descriptor_reg.valid && serial_read_engine_out_done_reg && serial_read_engine_out_ready_reg)
                     next_state = SETUP_KERNEL_REQ_START;
                 else
                     next_state = SETUP_KERNEL_IDLE;
             end
             SETUP_KERNEL_REQ_START : begin
-                if(~serial_read_engine_out_done_reg && ~serial_read_engine_out_ready_reg) begin
+                if(serial_read_engine_out_done_reg && serial_read_engine_out_ready_reg) begin
                     next_state = SETUP_KERNEL_REQ_START;
                 end else begin
                     next_state = SETUP_KERNEL_REQ_BUSY;
@@ -208,8 +208,8 @@ module glay_kernel_setup #(
             end
             SETUP_KERNEL_REQ_BUSY : begin
                 glay_setup_cache_req_out_din.valid           <= 1'b0;
-                kernel_setup_done                            <= serial_read_engine_out_done_reg & serial_read_engine_fifo_out_signals_reg.empty;
-                kernel_setup_start                           <= 1'b1;
+                kernel_setup_done                            <= serial_read_engine_out_done_reg & ~serial_read_engine_fifo_out_signals_reg.empty;
+                kernel_setup_start                           <= 1'b0;
                 serial_read_engine_fifo_in_signals_reg.rd_en <= ~serial_read_engine_fifo_out_signals_reg.empty;
                 serial_read_engine_in_start_reg              <= 1'b1;
                 serial_read_config_reg.valid                 <= 1'b1;
