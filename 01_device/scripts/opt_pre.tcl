691d76074172132d3dbba7aec37ac502cf7ce3ad
Abdullah <atmughrabi@gmail.com>
2023-04-20 02:58:15 -0400

    [SCRIPTS] add expand pblock

---
+++ 01_device/scripts/opt_pre.tcl
@@ -0,0 +1,6 @@
+#####################################
+# Tcl file used in OPT_DESIGN.TCL.PRE
+
+resize_pblock [get_pblocks  pblock_dynamic_SLR0] -add {SLR0}
+resize_pblock [get_pblocks  pblock_dynamic_SLR1] -add {SLR1}
+resize_pblock [get_pblocks  pblock_dynamic_SLR2] -add {SLR2}
