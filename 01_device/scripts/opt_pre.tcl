322c47ae1713e021b6a24af37ee201ffdeb87cbe
Abdullah <atmughrabi@gmail.com>
2023-04-20 15:51:23 -0400

    [GraphBlox] change cache requestor logic

--- 01_device/scripts/opt_pre.tcl
+++ 01_device/scripts/opt_pre.tcl
@@ -2,5 +2,5 @@
 # Tcl file used in OPT_DESIGN.TCL.PRE
 
 resize_pblock [get_pblocks  pblock_dynamic_SLR0] -add {SLR0}
-resize_pblock [get_pblocks  pblock_dynamic_SLR1] -add {SLR1}
-resize_pblock [get_pblocks  pblock_dynamic_SLR2] -add {SLR2}
+resize_pblock [get_pblocks  pblock_dynamic_SLR0] -add {SLR1}
+resize_pblock [get_pblocks  pblock_dynamic_SLR0] -add {SLR2}
