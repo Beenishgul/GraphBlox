c4621bfbd17c3f08300fe92528e7c5860cd73a25
Abdullah <atmughrabi@gmail.com>
2023-05-15 22:47:50 -0400

    [GLay|Scripts] refactor setup kernel engine

--- 01_device/scripts/generate_package_filelist_f.sh
+++ 01_device/scripts/generate_package_filelist_f.sh
@@ -1,7 +1,7 @@
 # @Author: Abdullah
 # @Date:   2023-04-06 18:46:46
 # @Last Modified by:   Abdullah
-# @Last Modified time: 2023-05-15 22:03:56
+# @Last Modified time: 2023-05-15 22:46:29
 #!/bin/bash
 
 
@@ -30,6 +30,7 @@ VIP_DIR=$5
 pkgs="pkg"
 
 engines="engines"
+engine_kernel_setup="engine_kernel_setup"
 engine_alu_operations="engine_alu_operations"
 engine_conditional="engine_conditional"
 engine_read="engine_read"
@@ -99,6 +100,7 @@ generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${vertex}/ ${CFG_FILE_NA
 generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${control}/ ${CFG_FILE_NAME} "sv"
 
 # generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${engines}/ ${CFG_FILE_NAME} "sv"
+generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${engines}/${engine_kernel_setup} ${CFG_FILE_NAME} "sv"
 generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${engines}/${engine_stride_index} ${CFG_FILE_NAME} "sv"
 # generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${engines}/${engine_read} ${CFG_FILE_NAME} "sv"
 # generate_package_filelist_f ${ACTIVE_APP_DIR}/${IP_DIR}/${engines}/${engine_write} ${CFG_FILE_NAME} "sv"
