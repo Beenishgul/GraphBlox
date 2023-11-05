132d51bb880b03bccfe8b696256b480697d5d1a3
Abdullah <atmughrabi@gmail.com>
2023-11-05 18:59:17 -0500

    [GraphBlox|IP|ENGINE] fix ALU gen

--- 02_device/rtl/engines/engine_alu_ops/engine_alu_ops_kernel.sv
+++ 02_device/rtl/engines/engine_alu_ops/engine_alu_ops_kernel.sv
@@ -31,72 +31,82 @@ module engine_alu_ops_kernel (
 );
 
   // Define internal signals
-  logic [  CACHE_FRONTEND_DATA_W-1:0] field_values[NUM_FIELDS_MEMORYPACKETDATA];
-  logic [CACHE_FRONTEND_DATA_W*2-1:0] alu_result                               ;
+  MemoryPacketData                      values_reg;
+  logic [(CACHE_FRONTEND_DATA_W*4)-1:0] result_int;
 
   // Process input data and mask
   always_ff @(posedge ap_clk) begin
     if (areset) begin
       for (int i = 0; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
-        field_values[i] <= 0;
+        values_reg.field[i] <= 0;
       end
     end else begin
       for (int i = 0; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
-        field_values[i] <= config_params.operate_on_constant&&config_params_valid?config_params.constant_value:data_valid?data.field[i] : 0;
+        values_reg.field[i] <= config_params.operate_on_constant&&config_params_valid?config_params.constant_value:data_valid?data.field[i] : 0;
       end
     end
   end
 
   // ALU operations logic
   always_comb begin
-    alu_result = 0;
     // Process the ALU operation if both config_params and data are valid
     if (config_params_valid && data_valid) begin
       case (config_params.alu_operation)
         ALU_NOP : begin
-          alu_result = field_values[0]; // No operation
+          result_int = values_reg.field[0]; // No operation
         end
 
         ALU_ADD : begin
-          alu_result = field_values[0];
+          result_int = values_reg.field[0];
           for (int i = 1; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
             if (config_params.alu_mask[i]) begin
-              alu_result = alu_result + field_values[i];
+              result_int = result_int + values_reg.field[i];
+            end else begin
+              result_int = result_int;
             end
           end
         end
 
         ALU_SUB : begin
-          alu_result = field_values[0];
+          result_int = values_reg.field[0];
           for (int i = 1; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
             if (config_params.alu_mask[i]) begin
-              alu_result = alu_result - field_values[i];
+              result_int = result_int - values_reg.field[i];
+            end else begin
+              result_int = result_int;
             end
           end
         end
 
         ALU_MUL : begin
-          // Assuming we're multiplying field[0] and field[1]
-          // Here, we assume synthesis tool will map this to DSP blocks.
-          alu_result = field_values[0] * field_values[1];
+          result_int = values_reg.field[0];
+          for (int i = 1; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
+            if (config_params.alu_mask[i]) begin
+              result_int = result_int * values_reg.field[i];
+            end else begin
+              result_int = result_int;
+            end
+          end
         end
 
         ALU_ACC : begin
-          alu_result = alu_result + field_values[0];
+          result_int = result_int + values_reg.field[0];
           for (int i = 1; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
             if (config_params.alu_mask[i]) begin
-              alu_result = alu_result + field_values[i];
+              result_int = result_int + values_reg.field[i];
+            end else begin
+              result_int = result_int;
             end
           end
         end
 
         ALU_DIV : begin
-          // Division logic
-          // Insert your division logic here, for example, using the non-restoring divider module
+          result_int = values_reg.field[0];
+          result_int = result_int >> values_reg.field[1]; // Undefined operations reset result
         end
 
         default : begin
-          alu_result = 0; // Undefined operations reset result
+          result_int = 0; // Undefined operations reset result
         end
       endcase
     end
@@ -110,7 +120,7 @@ module engine_alu_ops_kernel (
     end else begin
       // Assign the ALU result to the output MemoryPacketData
       for (int i = 0; i<NUM_FIELDS_MEMORYPACKETDATA; i++) begin
-        result.field[i] <= alu_result[CACHE_FRONTEND_DATA_W * i +: CACHE_FRONTEND_DATA_W];
+        result.field[i] <= result_int[CACHE_FRONTEND_DATA_W * i +: CACHE_FRONTEND_DATA_W];
       end
     end
   end
