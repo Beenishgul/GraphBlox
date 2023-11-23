d31759416daea95333d0a0853e9d98ae097866b2
Abdullah <atmughrabi@gmail.com>
2023-11-23 15:35:41 -0500

    [GraphBlox|IP] add engine json topology generation

---
+++ 02_device/utils/utils.py/generate_overlay_entries_json.py
@@ -0,0 +1,60 @@
+import json
+
+
+NUM_FIELDS_MEMORYPACKETDATA = 4;
+TYPE_MEMORY_CMD_BITS = 7;
+TYPE_FILTER_OPERATION_BITS = 9;
+TYPE_ALU_OPERATION_BITS = 6;
+TYPE_DATA_STRUCTURE_BITS = 6;
+TYPE_SEQUENCE_STATE_BITS = 4;
+
+def read_config(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def generate_hex_pattern(config):
+    # Logic to convert config to hex pattern
+    # This will depend on the specific format and calculation required.
+    pass
+
+# Function to compute bit manipulation for ALU_ops
+def compute_alu_ops(alu_operation, alu_mask, id_module, id_engine, const_mask, const_value, ops_mask, id_cu, id_bundle, id_lane, id_buffer):
+    # Compute bit manipulations based on Verilog logic
+    # Example: configure_memory_reg.payload.param.alu_operation <= alu_operation[TYPE_ALU_OPERATION_BITS-1:0]
+    alu_op_config = alu_operation & ((1 << TYPE_ALU_OPERATION_BITS) - 1)
+    # ... similar calculations for other parameters ...
+
+    return {
+        "alu_op_config": alu_op_config,
+        # ... other computed configurations ...
+    }
+
+# Example usage
+alu_ops_config = compute_alu_ops(alu_operation, alu_mask, id_module, id_engine, const_mask, const_value, ops_mask, id_cu, id_bundle, id_lane, id_buffer)
+
+def format_config_to_hex(config):
+    # Convert the configuration dictionary into a hex pattern
+    # This will depend on how these configurations are represented in hex
+    hex_pattern = ""
+    # Example: hex_pattern += "{:08x}".format(config["alu_op_config"])
+    # ... format other configurations ...
+
+    return hex_pattern
+
+# Format the ALU_ops configuration
+alu_ops_hex = format_config_to_hex(alu_ops_config)
+print(alu_ops_hex)
+
+
+def main():
+    config = read_config('config.json')
+    
+    # Example: Process ENGINE_ALU_OPS
+    alu_ops_config = config.get('ENGINE_ALU_OPS', {})
+    alu_ops_hex = generate_hex_pattern(alu_ops_config)
+    print(alu_ops_hex)
+    
+    # Process other configurations similarly...
+
+if __name__ == "__main__":
+    main()
