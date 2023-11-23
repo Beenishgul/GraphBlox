6bef23140a57e3128660662b412774d5ab045254
Abdullah <atmughrabi@gmail.com>
2023-11-22 21:37:30 -0500

    [GLAY|IP|ARBITER] add full synthesis script

---
+++ 02_device/utils/utils.sh/generate_algorithms_xclbin.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Define an array of algorithm indices to be processed
+# Add or remove indices as needed
+algorithm_indices=(0 1 5 6 8)
+
+# Loop through the specified algorithm indices
+for ALGORITHM_INDEX in "${algorithm_indices[@]}"
+do
+    # Export ALGORITHMS variable
+    export ALGORITHMS=$ALGORITHM_INDEX
+    export ARCHITECTURE=GLay
+    export CAPABILITY=Single
+    export XILINX_NUM_KERNELS=8
+
+    # Execute make and make build-hw in the background
+    # nohup ensures the process is not killed if the terminal is closed
+    # & puts the process in the background
+    # Output is redirected to a log file for each algorithm
+    nohup make && make build-hw > "algorithm_$ALGORITHM_INDEX.log" 2>&1 &
+done
+
+echo "Selected Single GLay algorithms are being processed in the background."
+
+    # Export ALGORITHMS variable
+    export ALGORITHMS=0
+    export ARCHITECTURE=GLay
+    export CAPABILITY=Lite
+    export XILINX_NUM_KERNELS=4
+
+    # Execute make and make build-hw in the background
+    # nohup ensures the process is not killed if the terminal is closed
+    # & puts the process in the background
+    # Output is redirected to a log file for each algorithm
+    nohup make && make build-hw > "algorithm_$ALGORITHM_INDEX.log" 2>&1 &
+
+echo "Selected Lite GLay algorithms are being processed in the background."
+
+    # Export ALGORITHMS variable
+    export ALGORITHMS=0
+    export ARCHITECTURE=GLay
+    export CAPABILITY=Full
+    export XILINX_NUM_KERNELS=2
+
+    # Execute make and make build-hw in the background
+    # nohup ensures the process is not killed if the terminal is closed
+    # & puts the process in the background
+    # Output is redirected to a log file for each algorithm
+    nohup make && make build-hw > "algorithm_$ALGORITHM_INDEX.log" 2>&1 &
+
+echo "Selected Full GLay algorithms are being processed in the background."
