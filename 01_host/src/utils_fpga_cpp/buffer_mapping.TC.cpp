2a3ab9b6c011228376789d25bfc634fb0262b2d8
Abdullah <atmughrabi@gmail.com>
2023-11-29 02:31:27 -0500

    [GLAY] simple push after checking BFS and PR

--- 01_host/src/utils_fpga_cpp/buffer_mapping.TC.cpp
+++ 01_host/src/utils_fpga_cpp/buffer_mapping.TC.cpp
@@ -2,18 +2,18 @@
 void GLAYGraphCSRxrtBufferHandlePerKernel::mapGLAYOverlayProgramBuffersTC(size_t overlay_program_entries, int algorithm, struct GraphCSR *graph, char *overlayPath)
 {
 // --------------------------------------------------------------------------------------
-// Name ENGINE_CSR_INDEX    ID 1    mapping 2    cycles 10   None-NONE ( 0 )-( graph->num_vertices )
+// Name ENGINE_CSR_INDEX    ID 1    mapping 2    cycles 10   buffer_5-edges_array_dest ( 0 )-( graph->num_edges )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
     overlay_program[1] = ( 0 );
    // --  2  - Index_End
-    overlay_program[2] = ( graph->num_vertices );
+    overlay_program[2] = ( graph->num_edges );
    // --  7  - Array_Pointer_LHS
-    overlay_program[7] = 0;
+    overlay_program[7] = xrt_buffer_device[5];
    // --  8  - Array_Pointer_RHS
-    overlay_program[8] = 0;
+    overlay_program[8] = xrt_buffer_device[5] >> 32;
    // --  9  - Array_size
-    overlay_program[9] = ( graph->num_vertices )-( 0 );
+    overlay_program[9] = ( graph->num_edges )-( 0 );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
 // Name ENGINE_FILTER_COND  ID 2    mapping 3    cycles 9    None-None ( 0 )-( 0 )
@@ -140,62 +140,62 @@ void GLAYGraphCSRxrtBufferHandlePerKernel::mapGLAYOverlayProgramBuffersTC(size_t
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 18   mapping 1    cycles 13   buffer_3-edges_idx ( 0 )-( graph->num_vertices )
+// Name ENGINE_READ_WRITE   ID 18   mapping 1    cycles 13   buffer_8-auxiliary_2 ( 0 )-( graph->num_vertices )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
     overlay_program[133] = ( 0 );
    // --  2  - Index_End
     overlay_program[134] = ( graph->num_vertices );
    // --  7  - Array_Pointer_LHS
-    overlay_program[139] = xrt_buffer_device[3];
+    overlay_program[139] = xrt_buffer_device[8];
    // --  8  - Array_Pointer_RHS
-    overlay_program[140] = xrt_buffer_device[3] >> 32;
+    overlay_program[140] = xrt_buffer_device[8] >> 32;
    // --  9  - Array_size
     overlay_program[141] = ( graph->num_vertices )-( 0 );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_MERGE_DATA   ID 19   mapping 4    cycles 2    None-None ( 0 )-( 0 )
-// --------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 20   mapping 1    cycles 13   buffer_2-out_degree ( 0 )-( graph->num_vertices )
+// Name ENGINE_READ_WRITE   ID 19   mapping 1    cycles 13   buffer_3-edges_idx ( 0 )-( graph->num_vertices )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
-    overlay_program[148] = ( 0 );
+    overlay_program[146] = ( 0 );
    // --  2  - Index_End
-    overlay_program[149] = ( graph->num_vertices );
+    overlay_program[147] = ( graph->num_vertices );
    // --  7  - Array_Pointer_LHS
-    overlay_program[154] = xrt_buffer_device[2];
+    overlay_program[152] = xrt_buffer_device[3];
    // --  8  - Array_Pointer_RHS
-    overlay_program[155] = xrt_buffer_device[2] >> 32;
+    overlay_program[153] = xrt_buffer_device[3] >> 32;
    // --  9  - Array_size
-    overlay_program[156] = ( graph->num_vertices )-( 0 );
+    overlay_program[154] = ( graph->num_vertices )-( 0 );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 21   mapping 1    cycles 13   buffer_3-edges_idx ( 0 )-( graph->num_vertices )
+// Name ENGINE_MERGE_DATA   ID 20   mapping 4    cycles 2    None-None ( 0 )-( 0 )
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+// Name ENGINE_READ_WRITE   ID 21   mapping 1    cycles 13   buffer_2-out_degree ( 0 )-( graph->num_vertices )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
     overlay_program[161] = ( 0 );
    // --  2  - Index_End
     overlay_program[162] = ( graph->num_vertices );
    // --  7  - Array_Pointer_LHS
-    overlay_program[167] = xrt_buffer_device[3];
+    overlay_program[167] = xrt_buffer_device[2];
    // --  8  - Array_Pointer_RHS
-    overlay_program[168] = xrt_buffer_device[3] >> 32;
+    overlay_program[168] = xrt_buffer_device[2] >> 32;
    // --  9  - Array_size
     overlay_program[169] = ( graph->num_vertices )-( 0 );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 22   mapping 1    cycles 13   buffer_8-auxiliary_2 ( 0 )-( graph->num_vertices )
+// Name ENGINE_READ_WRITE   ID 22   mapping 1    cycles 13   buffer_3-edges_idx ( 0 )-( graph->num_vertices )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
     overlay_program[174] = ( 0 );
    // --  2  - Index_End
     overlay_program[175] = ( graph->num_vertices );
    // --  7  - Array_Pointer_LHS
-    overlay_program[180] = xrt_buffer_device[8];
+    overlay_program[180] = xrt_buffer_device[3];
    // --  8  - Array_Pointer_RHS
-    overlay_program[181] = xrt_buffer_device[8] >> 32;
+    overlay_program[181] = xrt_buffer_device[3] >> 32;
    // --  9  - Array_size
     overlay_program[182] = ( graph->num_vertices )-( 0 );
 // --------------------------------------------------------------------------------------
