f93447065335c85a949c7d265983b94735c2397c
Abdullah <atmughrabi@gmail.com>
2023-12-14 23:52:16 -0500

    [GLAY|IP] add fifo arbiter sizes to shared parameter file

--- 02_device/rtl/utils/arbiter/arbiter_N_to_1_request.sv
+++ 02_device/rtl/utils/arbiter/arbiter_N_to_1_request.sv
@@ -18,8 +18,8 @@ module arbiter_N_to_1_request #(
   parameter NUM_MEMORY_REQUESTOR  = 2                               ,
   parameter NUM_ARBITER_REQUESTOR = 2**$clog2(NUM_MEMORY_REQUESTOR) ,
   parameter FIFO_ARBITER_DEPTH    = 16                              ,
-  parameter FIFO_WRITE_DEPTH      = 2**$clog2(FIFO_ARBITER_DEPTH+16),
-  parameter PROG_THRESH           = 2**$clog2(FIFO_ARBITER_DEPTH)
+  parameter FIFO_WRITE_DEPTH      = 2**$clog2(FIFO_ARBITER_DEPTH+32),
+  parameter PROG_THRESH           = 2**$clog2(16)
 ) (
   input  logic                            ap_clk                               ,
   input  logic                            areset                               ,
