55dd44203ec828a6cbfb60af1c7df9de03a32df2
Abdullah <atmughrabi@gmail.com>
2023-12-24 03:16:47 -0500

    [GraphBlox|IP] hook backtrack signals - fix compilation errors

--- 02_device/rtl/utils/arbiter/backtrack_fifo_lanes_response_signal.sv
+++ 02_device/rtl/utils/arbiter/backtrack_fifo_lanes_response_signal.sv
@@ -43,7 +43,8 @@ MemoryPacketArbitrate       configure_route_in_reg                              
 FIFOStateSignalsInput       fifo_response_engine_in_signals_in_reg                         ;
 FIFOStateSignalsOutput      fifo_response_lanes_backtrack_signals_in_reg[NUM_LANES_MAX-1:0];
 FIFOStateSignalsInput       fifo_response_engine_in_signals_out_reg                        ;
-logic [NUM_BUNDLES_MAX-1:0] next_module_id_bundle                                       ;
+logic [NUM_BUNDLES_MAX-1:0] next_module_id_bundle                                          ;
+logic [NUM_LANES_MAX-1:0]   signals_out_reg_rd_en                                          ;
 
 assign next_module_id_bundle = (1 << (ID_BUNDLE+1)) | ( 1 >> (NUM_BUNDLES_MAX-(ID_BUNDLE+1)));
 
@@ -90,17 +91,16 @@ end
 // --------------------------------------------------------------------------------------
 // Drive Response FIFO signals
 // --------------------------------------------------------------------------------------
-always_ff @(posedge ap_clk) begin
-    if(configure_route_valid_reg && (next_module_id_bundle == configure_route_in_reg.id_bundle[NUM_BUNDLES_MAX-1:0])) begin
+always_comb begin
+    signals_out_reg_rd_en = 0;
+    if(configure_route_valid_reg & (next_module_id_bundle == configure_route_in_reg.id_bundle[NUM_BUNDLES_MAX-1:0]) & (|configure_route_in_reg.id_lane)) begin
         for (int i = 0; i < NUM_LANES_MAX; i = i + 1) begin
-            if (configure_route_in_reg.id_lane == (1 << i)) begin
-                fifo_response_engine_in_signals_out_reg.rd_en <= fifo_response_engine_in_signals_in_reg.rd_en & ~fifo_response_lanes_backtrack_signals_in_reg[i].prog_full;
-            end
+            signals_out_reg_rd_en[i] = configure_route_in_reg.id_lane[i] ? ~fifo_response_lanes_backtrack_signals_in_reg[i].prog_full : 1'b1;
         end
-    end else begin
-        fifo_response_engine_in_signals_out_reg.rd_en <= 1'b0;;
-    end
+    end 
 end
+
+assign fifo_response_engine_in_signals_out_reg.rd_en = &signals_out_reg_rd_en & fifo_response_engine_in_signals_in_reg.rd_en;
 
 // --------------------------------------------------------------------------------------
 // Drive output
