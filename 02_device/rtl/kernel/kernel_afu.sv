c3ec09e305df6a1df30e093c55eb06dfc2f54ccc
Abdullah <atmughrabi@gmail.com>
2024-01-06 06:02:18 -0500

    [GLAY|IP|CU] added multi channel components generateion scripts for ports

--- 02_device/rtl/kernel/kernel_afu.sv
+++ 02_device/rtl/kernel/kernel_afu.sv
@@ -19,45 +19,8 @@ module kernel_afu (
   input  logic                         ap_clk         ,
   input  logic                         ap_rst_n       ,
   // AXI4 master interface m00_axi
-  output logic                         m00_axi_awvalid,
-  input  logic                         m00_axi_awready,
-  output logic [ M_AXI4_BE_ADDR_W-1:0] m00_axi_awaddr ,
-  output logic [  M_AXI4_BE_LEN_W-1:0] m00_axi_awlen  ,
-  output logic                         m00_axi_wvalid ,
-  input  logic                         m00_axi_wready ,
-  output logic [ M_AXI4_BE_DATA_W-1:0] m00_axi_wdata  ,
-  output logic [ M_AXI4_BE_STRB_W-1:0] m00_axi_wstrb  ,
-  output logic                         m00_axi_wlast  ,
-  input  logic                         m00_axi_bvalid ,
-  output logic                         m00_axi_bready ,
-  output logic                         m00_axi_arvalid,
-  input  logic                         m00_axi_arready,
-  output logic [ M_AXI4_BE_ADDR_W-1:0] m00_axi_araddr ,
-  output logic [  M_AXI4_BE_LEN_W-1:0] m00_axi_arlen  ,
-  input  logic                         m00_axi_rvalid ,
-  output logic                         m00_axi_rready ,
-  input  logic [ M_AXI4_BE_DATA_W-1:0] m00_axi_rdata  ,
-  input  logic                         m00_axi_rlast  ,
+ `include "m_axi_ports_afu.vh"
   // Control Signals
-  // AXI4 master interface m00_axi missing ports
-  input  logic [   M_AXI4_BE_ID_W-1:0] m00_axi_bid    ,
-  input  logic [   M_AXI4_BE_ID_W-1:0] m00_axi_rid    ,
-  input  logic [ M_AXI4_BE_RESP_W-1:0] m00_axi_rresp  ,
-  input  logic [ M_AXI4_BE_RESP_W-1:0] m00_axi_bresp  ,
-  output logic [   M_AXI4_BE_ID_W-1:0] m00_axi_awid   ,
-  output logic [ M_AXI4_BE_SIZE_W-1:0] m00_axi_awsize ,
-  output logic [M_AXI4_BE_BURST_W-1:0] m00_axi_awburst,
-  output logic [ M_AXI4_BE_LOCK_W-1:0] m00_axi_awlock ,
-  output logic [M_AXI4_BE_CACHE_W-1:0] m00_axi_awcache,
-  output logic [ M_AXI4_BE_PROT_W-1:0] m00_axi_awprot ,
-  output logic [  M_AXI4_BE_QOS_W-1:0] m00_axi_awqos  ,
-  output logic [   M_AXI4_BE_ID_W-1:0] m00_axi_arid   ,
-  output logic [ M_AXI4_BE_SIZE_W-1:0] m00_axi_arsize ,
-  output logic [M_AXI4_BE_BURST_W-1:0] m00_axi_arburst,
-  output logic [ M_AXI4_BE_LOCK_W-1:0] m00_axi_arlock ,
-  output logic [M_AXI4_BE_CACHE_W-1:0] m00_axi_arcache,
-  output logic [ M_AXI4_BE_PROT_W-1:0] m00_axi_arprot ,
-  output logic [  M_AXI4_BE_QOS_W-1:0] m00_axi_arqos  ,
   input  logic                         ap_start       ,
   output logic                         ap_idle        ,
   output logic                         ap_done        ,
@@ -195,62 +158,6 @@ always_ff @(posedge ap_clk) begin
 end
 
 // --------------------------------------------------------------------------------------
-// READ AXI4 SIGNALS INPUT
-// --------------------------------------------------------------------------------------
-assign m_axi4_read[0].in.rvalid  = m00_axi_rvalid ; // Read channel valid
-assign m_axi4_read[0].in.arready = m00_axi_arready; // Address read channel ready
-assign m_axi4_read[0].in.rlast   = m00_axi_rlast  ; // Read channel last word
-assign m_axi4_read[0].in.rdata   = swap_endianness_cacheline_axi_be(m00_axi_rdata, endian_read_reg)  ; // Read channel data
-assign m_axi4_read[0].in.rid     = m00_axi_rid    ; // Read channel ID
-assign m_axi4_read[0].in.rresp   = m00_axi_rresp  ; // Read channel response
-
-
-// --------------------------------------------------------------------------------------
-// READ AXI4 SIGNALS OUTPUT
-// --------------------------------------------------------------------------------------
-assign m00_axi_arvalid = m_axi4_read[0].out.arvalid; // Address read channel valid
-assign m00_axi_araddr  = m_axi4_read[0].out.araddr ; // Address read channel address
-assign m00_axi_arlen   = m_axi4_read[0].out.arlen  ; // Address write channel burst length
-assign m00_axi_rready  = m_axi4_read[0].out.rready ; // Read channel ready
-assign m00_axi_arid    = m_axi4_read[0].out.arid   ; // Address read channel ID
-assign m00_axi_arsize  = m_axi4_read[0].out.arsize ; // Address read channel burst size. This signal indicates the size of each transfer in the burst
-assign m00_axi_arburst = m_axi4_read[0].out.arburst; // Address read channel burst type
-assign m00_axi_arlock  = m_axi4_read[0].out.arlock ; // Address read channel lock type
-assign m00_axi_arcache = m_axi4_read[0].out.arcache; // Address read channel memory type. Transactions set with Normal Non-cacheable Modifiable and Bufferable (0011).
-assign m00_axi_arprot  = m_axi4_read[0].out.arprot ; // Address write channel protection type. Transactions set with Normal, Secure, and Data attributes (000).
-assign m00_axi_arqos   = m_axi4_read[0].out.arqos  ; // Address write channel quality of service
-
-
-// --------------------------------------------------------------------------------------
-// WRITE AXI4 SIGNALS INPUT
-// --------------------------------------------------------------------------------------
-assign m_axi4_write[0].in.awready = m00_axi_awready; // Address write channel ready
-assign m_axi4_write[0].in.wready  = m00_axi_wready ; // Write channel ready
-assign m_axi4_write[0].in.bid     = m00_axi_bid    ; // Write response channel ID
-assign m_axi4_write[0].in.bresp   = m00_axi_bresp  ; // Write channel response
-assign m_axi4_write[0].in.bvalid  = m00_axi_bvalid ; // Write response channel valid
-
-
-// --------------------------------------------------------------------------------------
-// WRITE AXI4 SIGNALS OUTPUT
-// --------------------------------------------------------------------------------------
-assign m00_axi_awvalid = m_axi4_write[0].out.awvalid; // Address write channel valid
-assign m00_axi_awid    = m_axi4_write[0].out.awid   ; // Address write channel ID
-assign m00_axi_awaddr  = m_axi4_write[0].out.awaddr ; // Address write channel address
-assign m00_axi_awlen   = m_axi4_write[0].out.awlen  ; // Address write channel burst length
-assign m00_axi_awsize  = m_axi4_write[0].out.awsize ; // Address write channel burst size. This signal indicates the size of each transfer in the burst
-assign m00_axi_awburst = m_axi4_write[0].out.awburst; // Address write channel burst type
-assign m00_axi_awlock  = m_axi4_write[0].out.awlock ; // Address write channel lock type
-assign m00_axi_awcache = m_axi4_write[0].out.awcache; // Address write channel memory type. Transactions set with Normal Non-cacheable Modifiable and Bufferable (0011).
-assign m00_axi_awprot  = m_axi4_write[0].out.awprot ; // Address write channel protection type. Transactions set with Normal, Secure, and Data attributes (000).
-assign m00_axi_awqos   = m_axi4_write[0].out.awqos  ; // Address write channel quality of service
-assign m00_axi_wdata   = swap_endianness_cacheline_axi_be(m_axi4_write[0].out.wdata , endian_write_reg); // Write channel data
-assign m00_axi_wstrb   = m_axi4_write[0].out.wstrb  ; // Write channel write strobe
-assign m00_axi_wlast   = m_axi4_write[0].out.wlast  ; // Write channel last word flag
-assign m00_axi_wvalid  = m_axi4_write[0].out.wvalid ; // Write channel valid
-assign m00_axi_bready  = m_axi4_write[0].out.bready ; // Write response channel ready
-
-// --------------------------------------------------------------------------------------
 // DRIVE DESCRIPTOR
 // --------------------------------------------------------------------------------------
 always_ff @(posedge ap_clk) begin
@@ -377,6 +284,11 @@ kernel_control inst_kernel_control (
   .descriptor_in (kernel_control_descriptor_in ),
   .descriptor_out(kernel_control_descriptor_out)
 );
+
+// --------------------------------------------------------------------------------------
+// Generate bundle MULTI channels generated
+// --------------------------------------------------------------------------------------
+`include "channel_topology.vh"
 
 endmodule : kernel_afu
 
