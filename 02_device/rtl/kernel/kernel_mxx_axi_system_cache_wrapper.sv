368106b0c0be878b190f90b78fd0e399a17d20a0
Abdullah <atmughrabi@gmail.com>
2024-01-11 02:42:34 -0500

    [GLAY|IP] Fix compilation errors

--- 02_device/rtl/kernel/kernel_mxx_axi_system_cache_wrapper.sv
+++ 02_device/rtl/kernel/kernel_mxx_axi_system_cache_wrapper.sv
@@ -9,7 +9,7 @@
 // Author : Abdullah Mughrabi atmughrabi@gmail.com/atmughra@virginia.edu
 // File   : kernel_mxx_axi_system_cache_wrapper.sv
 // Create : 2023-01-23 16:17:05
-// Revise : 2024-01-11 02:05:10
+// Revise : 2024-01-11 02:41:36
 // Editor : sublime text4, tab size (2)
 // -----------------------------------------------------------------------------
 
--- 02_device/rtl/utils/slice/mxx_axi_register_slice_mid_wrapper.sv
+++ 02_device/rtl/utils/slice/mxx_axi_register_slice_mid_wrapper.sv
@@ -9,7 +9,7 @@
 // Author : Abdullah Mughrabi atmughrabi@gmail.com/atmughra@virginia.edu
 // File   : mxx_axi_register_slice_mid_wrapper.sv
 // Create : 2023-01-23 16:17:05
-// Revise : 2024-01-11 02:05:10
+// Revise : 2024-01-11 02:41:36
 // Editor : sublime text4, tab size (2)
 // -----------------------------------------------------------------------------
 
