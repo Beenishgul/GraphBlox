9dfd08e4d9a6ff65066594078c02820441c743f5
Abdullah <atmughrabi@gmail.com>
2024-01-12 14:41:01 -0500

    [GLAY|IP] final topology for GLay

--- 02_device/rtl/utils/slice/mxx_axi_register_slice_mid_wrapper.sv
+++ 02_device/rtl/utils/slice/mxx_axi_register_slice_mid_wrapper.sv
@@ -9,7 +9,7 @@
 // Author : Abdullah Mughrabi atmughrabi@gmail.com/atmughra@virginia.edu
 // File   : mxx_axi_register_slice_mid_wrapper.sv
 // Create : 2023-01-23 16:17:05
-// Revise : 2024-01-12 14:31:44
+// Revise : 2024-01-12 14:40:12
 // Editor : sublime text4, tab size (2)
 // -----------------------------------------------------------------------------
 
