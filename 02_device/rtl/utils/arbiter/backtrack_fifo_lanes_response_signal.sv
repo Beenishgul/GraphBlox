fa74c8fa2570b471042cd8b352b1853f49eb0b01
Abdullah <atmughrabi@gmail.com>
2024-01-16 23:17:20 -0500

    [GraphBlox|IP|FIFO] change backtrack signal into combinational logic

--- 02_device/rtl/utils/arbiter/backtrack_fifo_lanes_response_signal.sv
+++ 02_device/rtl/utils/arbiter/backtrack_fifo_lanes_response_signal.sv
@@ -35,77 +35,34 @@ module backtrack_fifo_lanes_response_signal #(parameter
 // --------------------------------------------------------------------------------------
 // Wires and Variables
 // --------------------------------------------------------------------------------------
-logic areset_backtrack;
-
-logic                           configure_route_valid_reg                                            ;
-PacketRouteAddress              configure_route_in_reg                                               ;
-FIFOStateSignalsOutput          fifo_response_lanes_backtrack_signals_in_reg[NUM_BACKTRACK_LANES-1:0];
-FIFOStateSignalsInput           fifo_response_engine_in_signals_out_reg                              ;
-logic [        NUM_BUNDLES-1:0] next_module_id_bundle                                                ;
-logic [NUM_BACKTRACK_LANES-1:0] signals_out_reg_rd_en                                                ;
+logic [        NUM_BUNDLES-1:0] next_module_id_bundle;
+logic [NUM_BACKTRACK_LANES-1:0] signals_out_reg_rd_en;
 
 assign next_module_id_bundle = (1 << (ID_BUNDLE+1)) | ( 1 >> (NUM_BUNDLES-(ID_BUNDLE+1)));
 
-// --------------------------------------------------------------------------------------
-// Register reset signal
-// --------------------------------------------------------------------------------------
-always_ff @(posedge ap_clk) begin
-    areset_backtrack <= areset;
-end
-
-// --------------------------------------------------------------------------------------
-// Drive input signals
-// --------------------------------------------------------------------------------------
-always_ff @(posedge ap_clk) begin
-    if (areset_backtrack) begin
-        configure_route_valid_reg <= 1'b0;
-        configure_route_in_reg    <= 0;
-    end
-    else begin
-        if(configure_route_valid) begin
-            configure_route_valid_reg <= configure_route_valid;
-            configure_route_in_reg    <= configure_route_in;
-        end
-    end
-end
-
-// --------------------------------------------------------------------------------------
-// Drive output signals
-// --------------------------------------------------------------------------------------
-always_ff @(posedge ap_clk) begin
-    if (areset_backtrack) begin
-        fifo_response_engine_in_signals_out <= 0;
-    end else begin
-        fifo_response_engine_in_signals_out <= fifo_response_engine_in_signals_out_reg;
-    end
-end
-
-always_ff @(posedge ap_clk) begin
-    fifo_response_lanes_backtrack_signals_in_reg <= fifo_response_lanes_backtrack_signals_in;
-end
 
 // --------------------------------------------------------------------------------------
 // Drive Response FIFO signals
 // --------------------------------------------------------------------------------------
 always_comb begin
     signals_out_reg_rd_en = 0;
-    if(configure_route_valid_reg & (next_module_id_bundle == configure_route_in_reg.id_bundle[NUM_BUNDLES-1:0]) & (|configure_route_in_reg.id_lane)) begin
+    if(configure_route_valid & (next_module_id_bundle == configure_route_in.id_bundle[NUM_BUNDLES-1:0]) & (|configure_route_in.id_lane)) begin
         for (int i = 0; i < NUM_BACKTRACK_LANES; i = i + 1) begin
-            signals_out_reg_rd_en[i] = configure_route_in_reg.id_lane[i] ? ~fifo_response_lanes_backtrack_signals_in_reg[i].prog_full : 1'b1;
+            signals_out_reg_rd_en[i] = configure_route_in.id_lane[i] ? ~fifo_response_lanes_backtrack_signals_in[i].prog_full : 1'b1;
         end
     end
 
-    if(configure_route_valid_reg & (next_module_id_bundle != configure_route_in_reg.id_bundle[NUM_BUNDLES-1:0]) & (|configure_route_in_reg.id_lane)) begin
+    if(configure_route_valid & (next_module_id_bundle != configure_route_in.id_bundle[NUM_BUNDLES-1:0]) & (|configure_route_in.id_lane)) begin
         signals_out_reg_rd_en[NUM_BACKTRACK_LANES-2:0] = {(NUM_BACKTRACK_LANES-1){1'b1}};
-        signals_out_reg_rd_en[NUM_BACKTRACK_LANES-1]   = configure_route_in_reg.id_lane[NUM_BACKTRACK_LANES-1] ? ~fifo_response_lanes_backtrack_signals_in_reg[NUM_BACKTRACK_LANES-1].prog_full : 1'b1;
+        signals_out_reg_rd_en[NUM_BACKTRACK_LANES-1]   = configure_route_in.id_lane[NUM_BACKTRACK_LANES-1] ? ~fifo_response_lanes_backtrack_signals_in[NUM_BACKTRACK_LANES-1].prog_full : 1'b1;
     end
 
-    if(configure_route_valid_reg & ~(|configure_route_in_reg.id_lane)) begin
+    if(configure_route_valid & ~(|configure_route_in.id_lane)) begin
         signals_out_reg_rd_en[NUM_BACKTRACK_LANES-1:0] = {NUM_BACKTRACK_LANES{1'b1}};
     end
 end
 
-assign fifo_response_engine_in_signals_out_reg.rd_en = &signals_out_reg_rd_en;
+assign fifo_response_engine_in_signals_out.rd_en = &signals_out_reg_rd_en;
 
 // --------------------------------------------------------------------------------------
 // Drive output
