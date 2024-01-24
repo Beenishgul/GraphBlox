cab6356261e600aeee3718d336aaefbb5cad1016
Abdullah <atmughrabi@gmail.com>
2024-01-23 21:37:07 -0500

    [GraphBlox|IP|ENGINE] add sieve integration to topolgy files

--- 01_host/src/utils_fpga_cpp/buffer_mapping.BFS.cpp
+++ 01_host/src/utils_fpga_cpp/buffer_mapping.BFS.cpp
@@ -2,24 +2,24 @@
 void GLAYGraphCSRxrtBufferHandlePerKernel::mapGLAYOverlayProgramBuffersBFS(size_t overlay_program_entries, int algorithm, struct GraphCSR *graph, char *overlayPath)
 {
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 1    mapping 1    cycles 11   buffer_8-auxiliary_2 ( graph->num_vertices )-( graph->num_vertices * 2 )
+// Name ENGINE_READ_WRITE   ID 1    mapping 1    cycles 11   buffer_7-auxiliary_1 ( 0 )-( graph->num_vertices )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
-    overlay_program[1] = ( graph->num_vertices );
+    overlay_program[1] = ( 0 );
    // --  2  - Index_End
-    overlay_program[2] = ( graph->num_vertices * 2 );
+    overlay_program[2] = ( graph->num_vertices );
    // --  7  - Array_size
-    overlay_program[7] = ( graph->num_vertices * 2 )-( graph->num_vertices );
+    overlay_program[7] = ( graph->num_vertices )-( 0 );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 2    mapping 1    cycles 11   buffer_7-auxiliary_1 ( 0 )-( graph->num_vertices )
+// Name ENGINE_READ_WRITE   ID 2    mapping 1    cycles 11   buffer_7-auxiliary_1 ( graph->num_vertices )-( graph->num_vertices * 2 )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
-    overlay_program[12] = ( 0 );
+    overlay_program[12] = ( graph->num_vertices );
    // --  2  - Index_End
-    overlay_program[13] = ( graph->num_vertices );
+    overlay_program[13] = ( graph->num_vertices * 2 );
    // --  7  - Array_size
-    overlay_program[18] = ( graph->num_vertices )-( 0 );
+    overlay_program[18] = ( graph->num_vertices * 2 )-( graph->num_vertices );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
 // Name ENGINE_CSR_INDEX    ID 3    mapping 2    cycles 8    None-NONE ( 0 )-( graph->num_vertices )
@@ -64,7 +64,7 @@ void GLAYGraphCSRxrtBufferHandlePerKernel::mapGLAYOverlayProgramBuffersBFS(size_
     overlay_program[59] = ( graph->num_vertices )-( 0 );
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 9    mapping 1    cycles 11   buffer_7-auxiliary_1 ( 0 )-( graph->num_vertices )
+// Name ENGINE_READ_WRITE   ID 9    mapping 1    cycles 11   buffer_8-auxiliary_2 ( 0 )-( graph->num_vertices )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
     overlay_program[64] = ( 0 );
@@ -92,7 +92,7 @@ void GLAYGraphCSRxrtBufferHandlePerKernel::mapGLAYOverlayProgramBuffersBFS(size_
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
-// Name ENGINE_READ_WRITE   ID 13   mapping 1    cycles 11   buffer_7-auxiliary_1 ( graph->num_vertices )-( graph->num_vertices * 2 )
+// Name ENGINE_READ_WRITE   ID 13   mapping 1    cycles 11   buffer_8-auxiliary_2 ( graph->num_vertices )-( graph->num_vertices * 2 )
 // --------------------------------------------------------------------------------------
    // --  1  - Index_Start
     overlay_program[83] = ( graph->num_vertices );
