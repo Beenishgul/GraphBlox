9ba185eaeb3797ea3a17660ed10da24667d9723c
Abdullah <atmughrabi@gmail.com>
2024-02-18 06:00:29 -0500

    [GraphBlox|IP|ENGINE] fix parallel read/write engine stage

--- 02_device/rtl/engines/engine_parallel_read_write/engine_parallel_read_write_generator.sv
+++ 02_device/rtl/engines/engine_parallel_read_write/engine_parallel_read_write_generator.sv
@@ -29,42 +29,42 @@
 // uint32_t *csrIndexGenerator(uint32_t indexStart, uint32_t indexEnd, uint32_t granularity)
 
 module engine_parallel_read_write_generator #(parameter
-    ID_CU                      = 0                    ,
-    ID_BUNDLE                  = 0                    ,
-    ID_LANE                    = 0                    ,
-    ID_ENGINE                  = 0                    ,
-    ID_MODULE                  = 1                    ,
-    ENGINES_CONFIG             = 0                    ,
-    FIFO_WRITE_DEPTH           = 16                   ,
-    PROG_THRESH                = 8                    ,
-    PIPELINE_STAGES            = 2                    ,
-    COUNTER_WIDTH              = CACHE_FRONTEND_DATA_W,
-    NUM_BACKTRACK_LANES        = 4                    ,
-    NUM_CHANNELS               = 2                    ,
-    ENGINE_PARALLEL_CAST_WIDTH = 1                    ,
-    NUM_BUNDLES                = 4
+    ID_CU               = 0                    ,
+    ID_BUNDLE           = 0                    ,
+    ID_LANE             = 0                    ,
+    ID_ENGINE           = 0                    ,
+    ID_MODULE           = 1                    ,
+    ENGINES_CONFIG      = 0                    ,
+    FIFO_WRITE_DEPTH    = 16                   ,
+    PROG_THRESH         = 8                    ,
+    PIPELINE_STAGES     = 2                    ,
+    COUNTER_WIDTH       = CACHE_FRONTEND_DATA_W,
+    NUM_BACKTRACK_LANES = 4                    ,
+    NUM_CHANNELS        = 2                    ,
+    ENGINE_CAST_WIDTH   = 1                    ,
+    NUM_BUNDLES         = 4
 ) (
     // System Signals
-    input  logic                          ap_clk                                                                                      ,
-    input  logic                          areset                                                                                      ,
-    input  ParallelReadWriteConfiguration configure_memory_in                                                                         ,
-    input  FIFOStateSignalsInput          fifo_configure_memory_in_signals_in                                                         ,
-    input  EnginePacket                   response_engine_in                                                                          ,
-    input  FIFOStateSignalsInput          fifo_response_engine_in_signals_in                                                          ,
-    output FIFOStateSignalsOutput         fifo_response_engine_in_signals_out                                                         ,
-    input  FIFOStateSignalsOutput         fifo_response_lanes_backtrack_signals_in[NUM_BACKTRACK_LANES+ENGINE_PARALLEL_CAST_WIDTH-1:0],
-    input  MemoryPacketResponse           response_memory_in                                                                          ,
-    input  FIFOStateSignalsInput          fifo_response_memory_in_signals_in                                                          ,
-    output FIFOStateSignalsOutput         fifo_response_memory_in_signals_out                                                         ,
-    output EnginePacket                   request_engine_out                                                                          ,
-    input  FIFOStateSignalsInput          fifo_request_engine_out_signals_in                                                          ,
-    output FIFOStateSignalsOutput         fifo_request_engine_out_signals_out                                                         ,
-    output MemoryPacketRequest            request_memory_out                                                                          ,
-    input  FIFOStateSignalsInput          fifo_request_memory_out_signals_in                                                          ,
-    output FIFOStateSignalsOutput         fifo_request_memory_out_signals_out                                                         ,
-    input  FIFOStateSignalsOutput         fifo_request_memory_out_backtrack_signals_in[NUM_CHANNELS-1:0]                              ,
-    output logic                          fifo_setup_signal                                                                           ,
-    output logic                          configure_memory_setup                                                                      ,
+    input  logic                          ap_clk                                                                             ,
+    input  logic                          areset                                                                             ,
+    input  ParallelReadWriteConfiguration configure_memory_in                                                                ,
+    input  FIFOStateSignalsInput          fifo_configure_memory_in_signals_in                                                ,
+    input  EnginePacket                   response_engine_in                                                                 ,
+    input  FIFOStateSignalsInput          fifo_response_engine_in_signals_in                                                 ,
+    output FIFOStateSignalsOutput         fifo_response_engine_in_signals_out                                                ,
+    input  FIFOStateSignalsOutput         fifo_response_lanes_backtrack_signals_in[NUM_BACKTRACK_LANES+ENGINE_CAST_WIDTH-1:0],
+    input  MemoryPacketResponse           response_memory_in                                                                 ,
+    input  FIFOStateSignalsInput          fifo_response_memory_in_signals_in                                                 ,
+    output FIFOStateSignalsOutput         fifo_response_memory_in_signals_out                                                ,
+    output EnginePacket                   request_engine_out                                                                 ,
+    input  FIFOStateSignalsInput          fifo_request_engine_out_signals_in                                                 ,
+    output FIFOStateSignalsOutput         fifo_request_engine_out_signals_out                                                ,
+    output MemoryPacketRequest            request_memory_out                                                                 ,
+    input  FIFOStateSignalsInput          fifo_request_memory_out_signals_in                                                 ,
+    output FIFOStateSignalsOutput         fifo_request_memory_out_signals_out                                                ,
+    input  FIFOStateSignalsOutput         fifo_request_memory_out_backtrack_signals_in[NUM_CHANNELS-1:0]                     ,
+    output logic                          fifo_setup_signal                                                                  ,
+    output logic                          configure_memory_setup                                                             ,
     output logic                          done_out
 );
 
@@ -171,11 +171,11 @@ module engine_parallel_read_write_generator #(parameter
 // --------------------------------------------------------------------------------------
 // Backtrack FIFO module - Bundle i <- Bundle i-1
 // --------------------------------------------------------------------------------------
-    logic                  areset_backtrack                                                                                      ;
-    logic                  backtrack_configure_route_valid                                                                       ;
-    PacketRouteAddress     backtrack_configure_route_in                                                                          ;
-    FIFOStateSignalsOutput backtrack_fifo_response_lanes_backtrack_signals_in[NUM_BACKTRACK_LANES+ENGINE_PARALLEL_CAST_WIDTH-1:0];
-    FIFOStateSignalsInput  backtrack_fifo_response_engine_in_signals_out                                                         ;
+    logic                  areset_backtrack                                                                             ;
+    logic                  backtrack_configure_route_valid                                                              ;
+    PacketRouteAddress     backtrack_configure_route_in                                                                 ;
+    FIFOStateSignalsOutput backtrack_fifo_response_lanes_backtrack_signals_in[NUM_BACKTRACK_LANES+ENGINE_CAST_WIDTH-1:0];
+    FIFOStateSignalsInput  backtrack_fifo_response_engine_in_signals_out                                                ;
 
 // --------------------------------------------------------------------------------------
 // Backtrack FIFO module - Engine i <- Channel i-1
@@ -592,14 +592,14 @@ module engine_parallel_read_write_generator #(parameter
     assign backtrack_fifo_response_lanes_backtrack_signals_in = fifo_response_lanes_backtrack_signals_in;
 
     backtrack_fifo_lanes_response_signal #(
-        .ID_CU                     (ID_CU                     ),
-        .ID_BUNDLE                 (ID_BUNDLE                 ),
-        .ID_LANE                   (ID_LANE                   ),
-        .ID_ENGINE                 (ID_ENGINE                 ),
-        .ID_MODULE                 (2                         ),
-        .NUM_BACKTRACK_LANES       (NUM_BACKTRACK_LANES       ),
-        .ENGINE_PARALLEL_CAST_WIDTH(ENGINE_PARALLEL_CAST_WIDTH),
-        .NUM_BUNDLES               (NUM_BUNDLES               )
+        .ID_CU              (ID_CU              ),
+        .ID_BUNDLE          (ID_BUNDLE          ),
+        .ID_LANE            (ID_LANE            ),
+        .ID_ENGINE          (ID_ENGINE          ),
+        .ID_MODULE          (2                  ),
+        .NUM_BACKTRACK_LANES(NUM_BACKTRACK_LANES),
+        .ENGINE_CAST_WIDTH  (ENGINE_CAST_WIDTH  ),
+        .NUM_BUNDLES        (NUM_BUNDLES        )
     ) inst_backtrack_fifo_lanes_response_signal (
         .ap_clk                                  (ap_clk                                            ),
         .areset                                  (areset_backtrack                                  ),
