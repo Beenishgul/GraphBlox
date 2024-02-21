ee8ebcd0a225498c4a4971402d234e8b0cef957c
Abdullah <atmughrabi@gmail.com>
2024-02-21 14:31:09 -0500

    [GraphBlox|IP|ENGINE] Fix combinational loops attempt-5 test for multi-lane parallel read/writes

--- 02_device/rtl/utils/counter/counter_pulse.sv
+++ 02_device/rtl/utils/counter/counter_pulse.sv
@@ -142,8 +142,8 @@ module config_params_select_pulse #(parameter int MASK_WIDTH = 8 // Default para
         param_select_flag[0] = 1'b1;
         param_valid_flag[0]  = param_select_out[0] | config_params_lane_valid[0];
         for (int i = 1; i < MASK_WIDTH; i++) begin
-            param_select_flag[i] = ((param_select_out[i] | config_params_lane_valid[i]) & ~config_params_kernel_valid_int[i-1]);
-            param_valid_flag[i]  = (config_params_kernel_valid_int[i-1] | param_select_flag[i]);
+            param_select_flag[i] = ((param_select_out[i] | config_params_lane_valid[i]) & ~param_valid_flag[i-1]);
+            param_valid_flag[i]  = (param_valid_flag[i-1] | param_select_flag[i]);
         end
     end
 
