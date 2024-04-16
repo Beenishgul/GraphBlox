1feb5073167c386cfc4e7818f3ebfcc1016df3bf
Abdullah <atmughrabi@gmail.com>
2024-04-16 16:05:35 -0400

    [GraphBlox|IP|RUN] reduce fields to 4 instead of 5

--- 02_device/rtl/engines/engine_parallel_read_write/engine_parallel_read_write_kernel.sv
+++ 02_device/rtl/engines/engine_parallel_read_write/engine_parallel_read_write_kernel.sv
@@ -51,21 +51,16 @@ end
 
 always_ff @(posedge ap_clk) begin
   for (int i = 0; i<ENGINE_PACKET_DATA_NUM_FIELDS; i++) begin
-    if(config_params_in.const_mask[i]) begin
-      org_value_reg.field[i]       <= config_params_in.const_value;
-      org_value_reg.field_state[i] <= SEQUENCE_RUNNING;
-    end else  begin
-      if(|config_params_in.ops_mask[i])begin
-        for (int j = 0; j<ENGINE_PACKET_DATA_NUM_FIELDS; j++) begin
-          if(config_params_in.ops_mask[i][j]) begin
-            org_value_reg.field[i]       <= data_in.field[j];
-            org_value_reg.field_state[i] <= data_in.field_state[j];
-          end
+    if(|config_params_in.ops_mask[i])begin
+      for (int j = 0; j<ENGINE_PACKET_DATA_NUM_FIELDS; j++) begin
+        if(config_params_in.ops_mask[i][j]) begin
+          org_value_reg.field[i]       <= data_in.field[j];
+          org_value_reg.field_state[i] <= data_in.field_state[j];
         end
-      end else begin
-        org_value_reg.field[i]       <= data_in.field[i];
-        org_value_reg.field_state[i] <= data_in.field_state[i];
       end
+    end else begin
+      org_value_reg.field[i]       <= data_in.field[i];
+      org_value_reg.field_state[i] <= data_in.field_state[i];
     end
   end
 end
